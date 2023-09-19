8990e1597e8a7f03a0570529e4c510acf8c52915
JS-project-01 <brunostefano1003@gmail.com>
2023-09-19 22:01:18 +0200

    Make the cart quantity interactive

--- javascript-amazon-project-main/scripts/amazon.js
+++ javascript-amazon-project-main/scripts/amazon.js
@@ -78,6 +78,16 @@ document.querySelectorAll(".js-add-to-cart")
                 quantity: 1
             });
         }
-            console.log(cart);
+          
+        let cartQuantity = 0;
+        cart.forEach((item) => {
+            cartQuantity += item.quantity;
         });
+        console.log(cartQuantity),
+        console.log(cart);
+        
+        document.querySelector(".js-cart-quantity")
+            .innerHTML = cartQuantity;
+        });
+
     });
